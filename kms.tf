b7f55c52cca17b6261b544930adec9e26f05d84b
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2020-01-09 14:29:57 -0500

    refactor file names and add tfsec

---
+++ kms.tf
@@ -0,0 +1,15 @@
+data "template_file" "cloudtrail_key_policy" {
+  template = file("${path.module}/files/cloudtrail_key_policy.tpl.json")
+  vars = {
+    account_id = data.aws_caller_identity.current.account_id
+    region     = data.aws_region.current.name
+  }
+}
+
+# Create KMS Key
+resource "aws_kms_key" "cloudtrail" {
+  description         = "A KMS key to encrypt CloudTrail events."
+  enable_key_rotation = "true"
+
+  policy = data.template_file.cloudtrail_key_policy.rendered
+}
